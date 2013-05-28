ec81e99e2174dc129ff72966262beddddc033662
Zach Jones <prozachj@gmail.com>
2013-05-28 01:38:23 -0500

    rename domainRoutes to router.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 
 var app = express();
-var Routes = require('./routes/domainRoutes');
+var Routes = require('./routes/router');
 
 //middleware order matters
 app.use(express.static(__dirname + '/static'));

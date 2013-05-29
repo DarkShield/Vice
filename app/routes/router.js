30bfeb6f0522c2b195c91bcf26ef1b99cbb1df64
Zach Jones <prozachj@gmail.com>
2013-05-29 00:38:32 -0500

    add redirect on failed login to include ?reason&err

--- app/routes/router.js
+++ app/routes/router.js
@@ -2,12 +2,6 @@ var fs = require('fs'),
     userM = require('../model/user'),
     reqModel = require('../../lib/requestSchema');
 
-//exports.getUsersDomains = getUsersDomains;
-//
-/*function getUsersDomains(req, res){
-	//TODO how to query for a list of sites
-	var dbQuery = reqModel.find( { '$or': [ { 'headers.host':   }, {   } ] }, function (err, docs){res.send(docs);} );
-}*/
 exports.login = function (req, res) {
   res.sendfile('./public/html/login.html');
 }
@@ -16,29 +10,17 @@ exports.login = function (req, res) {
 exports.dashboard = getAuth;
 
 function getAuth(req, res){
-	console.log(req.param('username')+' & '+req.param('password'));
-	userM.getAuthenticated(req.param('username'), req.param('password'), function(err, user, reason){
-		if (user !== null) {
-			req.session.user = user;
-			res.sendfile('./routes/html/dashboard.html');
-
-			/*fs.readFile('./static/html/dashboard.html', function(err, content) {
-				if (err){
-					res.status(404);
-					console.log(err);
-				}
-				else {
-					res.status(200);
-					console.log('success');
-				}
-			});*/
-		}
-		else {
-			console.log('user: '+user);
-			console.log('error: '+err);
-			console.log('reason: '+reason);
-		}
-	});	
+  var username = req.param('username');
+  var password = req.param('password');
+  var respond = function (err, user, reason) {
+    if (user !==null){
+      req.session.user = user;
+      res.sendfile('./routes/html/dashboard.html');
+    } else {
+      res.redirect('/login?' + reason + '&' + err );
+    }
+  }
+  userM.getAuthenticated(username, password, respond);
 }
 
 
